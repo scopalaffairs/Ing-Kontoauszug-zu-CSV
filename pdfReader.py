a2ac04c03048b45afe7938a227f68b71816c3b04
scopalaffairs <ministry@scopal-affairs.com>
2024-12-17 07:46:21 +0100

    forked repo

--- pdfReader.py
+++ pdfReader.py
@@ -1,74 +1,100 @@
-import pdfplumber
-import re
-import csv
-import os
-import sys
-import getopt
+#!/usr/bin/env python3
 
-keywords = ['Lastschrift', 'Ueberweisung', 'Gutschrift', 'Entgelt', 'Gehalt/Rente']
+import csv
+import getopt
+import os
+import re
+import sys
+
+import pdfplumber
+
+keywords = ["Lastschrift", "Ueberweisung", "Gutschrift", "Entgelt", "Gehalt/Rente"]
 
 
 def convert_file(input_file):
     path, file_name = os.path.split(input_file)
-    output_file = path + '/' + file_name.split('.')[0] + '.csv'
-    with pdfplumber.open(input_file) as pdf:
-        file = open(output_file, 'w')
-        writer = csv.writer(file)
-        writer.writerow(['date', 'amount', 'category', 'purpose'])
+    output_file = os.path.join(path, f"{os.path.splitext(file_name)[0]}.csv")
 
-        for idx, page in enumerate(pdf.pages):
-            if idx == len(pdf.pages) - 1:
-                continue
-            txt = page.extract_text().split('\n')
+    with pdfplumber.open(input_file) as pdf, open(output_file, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            [
+                "Date",
+                "Amount (Credit)",
+                "Amount (Debit)",
+                "Category Name",
+                "purpose",
+            ]
+        )
+
+        for idx, page in enumerate(pdf.pages[:-1]):
+            txt = page.extract_text().split("\n")
             for line in txt:
-                reg_erg = re.search('\d{2}(\.){1}\d{2}(\.){1}\d{4}', line)
-                if bool(reg_erg) and bool([ele for ele in keywords if (ele in line)]):
-                    if reg_erg.start() != 0:
-                        line = line[reg_erg.start():]
-                    date = reg_erg.group()
-                    amount = re.search('-*\d*(\.)*\d+,{1}\d{2}', line).group().replace('.', '').replace(',', '.')
-                    line = line.replace(date, '').replace(re.search('-*\d*\.*\d+,{1}\d{2}', line).group(), '').replace(
-                        ',', ';')
+                date_match = re.search(r"\d{2}\.\d{2}\.\d{4}", line)
+                keyword_found = any(keyword in line for keyword in keywords)
+
+                if date_match and keyword_found:
+                    if date_match.start() != 0:
+                        line = line[date_match.start() :]
+                    date = date_match.group()
+
+                    amount_match = re.search(r"-?\d+\.?\d*,\d{2}", line)
+                    if amount_match:
+                        amount = amount_match.group().replace(".", "").replace(",", ".")
+                    else:
+                        amount = "0.00"
+
+                    line = (
+                        line.replace(date, "")
+                        .replace(amount_match.group() if amount_match else "", "")
+                        .replace(",", ";")
+                    )
+
                     category, purpose = get_category_purpose(line)
-                    #print(date + ', ' + amount + ', ' + category + ', ' + purpose)
+
                     writer.writerow([date, amount, category, purpose])
-        file.close()
 
 
 def get_category_purpose(line):
-    category = 'undefined'
-    purpose = 'undefined'
+    """
+    Determines the category and purpose from a line based on keywords.
+    """
+    category = "undefined"
+    purpose = "undefined"
     for key in keywords:
         if key in line:
             category = key
-            purpose = line.replace(key, '').strip()
+            purpose = line.replace(key, "").strip()
+            break
     return category, purpose
 
 
 if __name__ == "__main__":
-    user_provided_path = '/Users/...'
+    user_provided_path = os.getcwd()
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hi:")#, ["ifile=", "ofile="])
+        opts, args = getopt.getopt(sys.argv[1:], "hi:")
     except getopt.GetoptError:
-        print('you need to pass an argument in the form: test.py -i <inputfile>')
+        print("you need to pass an argument in the form: test.py -i <inputfile>")
         sys.exit(2)
     for opt, arg in opts:
-        if opt == '-h':
-            print('test.py -i <inputfile>')
+        if opt == "-h":
+            print("test.py -i <inputfile>")
             sys.exit()
-        elif opt == '-i':
+        elif opt == "-i":
             user_provided_path = arg
-    print('Input file is: ', user_provided_path)
+    print("Input file is: ", user_provided_path)
 
     if os.path.isdir(user_provided_path):
         for filename in os.listdir(user_provided_path):
-            print(user_provided_path + '/' + filename)
-            convert_file(user_provided_path + '/' + filename)
+            print(user_provided_path + "/" + filename)
+            convert_file(user_provided_path + "/" + filename)
         print("folder converted sucessfully")
     elif os.path.isfile(user_provided_path):
         convert_file(user_provided_path)
         print("file converted succesfully")
     else:
-        print("Ooops something went wrong. Check that the path you provide is valid and redirects either to a .pdf or "
-              "a directory with only pdfs.")
+        print(
+            "Something went wrong. Check that the path you provide is valid and redirects either to a .pdf or "
+            "a directory with only pdfs."
+        )
     sys.exit()
